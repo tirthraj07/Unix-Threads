8dfd6a7e0a62a44fbf2ab6c85af95ba74c8aea9b
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-23 02:10:04 +0530

    Race Conditions

--- main.cpp
+++ main.cpp
@@ -3,53 +3,52 @@
 #include <pthread.h>
 using namespace std;
 
-int x = 2;
+int x = 0;
+int ITERATIONS = 1000000;
 
 /*
 OUTPUT:
-Routine 1: Value of x is 3
-Process ID: 549
+Value of x = 128758
+Expected : 200000
+Missed % : 35.621
 
-Routine 2: Value of x is 3
-Process ID: 549
+OUTPUT:
+Value of x = 1046202
+Expected : 2000000
+Missed % : 47.6899
 
-
-This clearly shows that threads share the same memory space and the process is the same.
-Where as fork() would create an entirely new process and copy all the variables
+This is called Race Condition
 */
 
-void *routine1(void *)
+void *routine(void *)
 {
-	x++;
-	cout << "\nRoutine 1: Value of x is " << x << endl;
-	cout << "Process ID: " << getpid() << endl;
-	return nullptr;
-}
-
-void *routine2(void *)
-{
-	sleep(2);
-	cout << "\nRoutine 2: Value of x is " << x << endl;
-	cout << "Process ID: " << getpid() << endl;
+	for (int i = 0; i < ITERATIONS; i++)
+	{
+		x++;
+	}
 	return nullptr;
 }
 
 int main()
 {
 	pthread_t t1, t2;
-	if (pthread_create(&t1, NULL, &routine1, NULL) != 0)
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
 	{
 		cout << "Unable to create thread t1" << endl;
 		return EXIT_FAILURE;
 	};
 
-	if (pthread_create(&t2, NULL, &routine2, NULL) != 0)
+	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
 	{
 		cout << "Unable to create thread t2" << endl;
 		return EXIT_FAILURE;
 	};
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
+
+	cout << "Value of x = " << x << endl;
+	cout << "Expected : " << ITERATIONS * 2 << endl;
+	cout << "Missed % : " << ((float)(ITERATIONS * 2 - x) / (float)(ITERATIONS * 2)) * 100 << endl;
 
 	return 0;
 }
