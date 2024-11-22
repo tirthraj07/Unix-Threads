c231ecc039b15938bf892a8c0c9d54023eb5e380
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-23 01:55:09 +0530

    Difference between processes and threads

--- main.cpp
+++ main.cpp
@@ -3,24 +3,47 @@
 #include <pthread.h>
 using namespace std;
 
-void *routine(void *)
+int x = 2;
+
+/*
+OUTPUT:
+Routine 1: Value of x is 3
+Process ID: 549
+
+Routine 2: Value of x is 3
+Process ID: 549
+
+
+This clearly shows that threads share the same memory space and the process is the same.
+Where as fork() would create an entirely new process and copy all the variables
+*/
+
+void *routine1(void *)
 {
-	cout << "Test from threads" << endl;
-	sleep(3);
-	cout << "Ending Thread" << endl;
+	x++;
+	cout << "\nRoutine 1: Value of x is " << x << endl;
+	cout << "Process ID: " << getpid() << endl;
+	return nullptr;
+}
+
+void *routine2(void *)
+{
+	sleep(2);
+	cout << "\nRoutine 2: Value of x is " << x << endl;
+	cout << "Process ID: " << getpid() << endl;
 	return nullptr;
 }
 
 int main()
 {
 	pthread_t t1, t2;
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+	if (pthread_create(&t1, NULL, &routine1, NULL) != 0)
 	{
 		cout << "Unable to create thread t1" << endl;
 		return EXIT_FAILURE;
 	};
 
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+	if (pthread_create(&t2, NULL, &routine2, NULL) != 0)
 	{
 		cout << "Unable to create thread t2" << endl;
 		return EXIT_FAILURE;
