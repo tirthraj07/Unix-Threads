83bea6673ff2fd15bf99f9ad06e48200f8cf307b
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-11-23 02:41:04 +0530

    How to create threads in a loop (pthread_create)

--- main.cpp
+++ main.cpp
@@ -4,19 +4,14 @@
 using namespace std;
 
 int x = 0;
-int ITERATIONS = 100000000;
+int ITERATIONS = 10000000;
 
 pthread_mutex_t mutex;
 
 /*
 OUTPUT:
-Value of x = 2000000
-Expected : 2000000
-Missed % : 0
-
-OUTPUT:
-Value of x = 200000000
-Expected : 200000000
+Value of x = 40000000
+Expected : 40000000
 Missed % : 0
 
 Thus we can avoid race conditions using mutex locks
@@ -35,26 +30,26 @@ void *routine(void *)
 
 int main()
 {
-	pthread_t t1, t2;
-	pthread_mutex_init(&mutex, NULL); // Reserve the memory for mutex variable
+	pthread_t th[4];
+	pthread_mutex_init(&mutex, NULL);
 
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+	for (int i = 0; i < 4; i++)
 	{
-		cout << "Unable to create thread t1" << endl;
-		return EXIT_FAILURE;
-	};
+		if (pthread_create(&th[i], NULL, &routine, NULL))
+		{
+			cout << "Could not create thread " << i + 1 << endl;
+			return EXIT_FAILURE;
+		}
+	}
 
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+	for (int i = 0; i < 4; i++)
 	{
-		cout << "Unable to create thread t2" << endl;
-		return EXIT_FAILURE;
-	};
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
+		pthread_join(th[i], NULL);
+	}
 
 	cout << "Value of x = " << x << endl;
-	cout << "Expected : " << ITERATIONS * 2 << endl;
-	cout << "Missed % : " << ((float)(ITERATIONS * 2 - x) / (float)(ITERATIONS * 2)) * 100 << endl;
+	cout << "Expected : " << ITERATIONS * 4 << endl;
+	cout << "Missed % : " << ((float)(ITERATIONS * 4 - x) / (float)(ITERATIONS * 4)) * 100 << endl;
 
 	pthread_mutex_destroy(&mutex); // Free the memory
 	return 0;
